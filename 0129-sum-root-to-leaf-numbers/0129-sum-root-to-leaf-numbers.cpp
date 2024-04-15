6dc69825a1664d42fceebc40918b910e50072ce7
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-04-15 12:14:45 +0200

    Time: 0 ms (100.00%), Space: 11.9 MB (9.00%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -12,30 +12,6 @@
 class Solution {
 public:
     vector<string> v;
-    string add(string a, string b) {
-        if (a.length() > b.length())
-            swap(a, b);
-        string str = "";
-        int n = a.length(), m = b.length();
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-        int carry = 0;
-        for (int i = 0; i < n; i++) {
-            int sum = ((a[i] - '0') + (b[i] - '0') + carry);
-            str.push_back(sum % 10 + '0');
-            carry = sum / 10;
-        }
-        for (int i = n; i < m; i++) {
-            int sum = ((b[i] - '0') + carry);
-            str.push_back(sum % 10 + '0');
-            carry = sum / 10;
-        }
-        if (carry)
-            str.push_back(carry + '0');
-        reverse(str.begin(), str.end());
-        return str;
-    }
-    
     void dfs(TreeNode* root, string a){
         if(!root -> left and !root -> right){
             v.push_back(a + char(root -> val + '0'));
@@ -49,8 +25,8 @@ public:
     
     int sumNumbers(TreeNode* root) {
         dfs(root, "");
-        string ret = "";
-        for(auto a : v) ret = add(ret, a);
-        return stoi(ret);
+        int ret = 0;
+        for(auto a : v) ret += stoi(a);
+        return ret;
     }
 };
