dae607082adef8fc330f621f169082d49aa7c515
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-09-12 20:43:54 +0300

    Time: 390 ms (5.10%), Space: 63.1 MB (5.10%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -4,26 +4,19 @@
  * @return {number}
  */
 var countConsistentStrings = function(allowed, words) {
+
     let appear = {};
 
-    for (let i in allowed) {
-      appear[allowed[i]] = true;
-    }
+    for (let i in allowed) appear[allowed[i]] = true;
 
     let answer = 0;
 
     words.forEach((word) => {
-      let isAllowed = true;
-      for (let i in word) {
-        if (!appear[word[i]]) {
-          isAllowed = false;
-          break;
-        }
-      }
-      if (isAllowed) {
-        answer++;
-      }
+      let isAllowed = 1;
+      for (let i in word) isAllowed &= appear[word[i]];
+      answer += isAllowed;
     });
 
     return answer;
+
 };
