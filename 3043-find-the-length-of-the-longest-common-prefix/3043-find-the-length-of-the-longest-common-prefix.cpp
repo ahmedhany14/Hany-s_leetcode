40b60890ada7e2306c8c4b926c5e22b2207a9e10
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-09-24 06:30:40 +0300

    Time: 2990 ms (5.11%), Space: 342.6 MB (5.00%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        map<string, int> f1, f2;
+        for(auto &i : arr1){
+            string a = to_string(i), b = "";
+            for(auto c: a){
+                b += c;
+                f1[b] = 1;
+            }
+        }
+        for(auto &i : arr2){
+            string a = to_string(i), b = "";
+            for(auto c: a){
+                b += c;
+                f2[b] = 1;
+            }
+        }
+        
+        int answer = 0;
+        
+        for(auto &i : arr1){
+            string a = to_string(i), b = "";
+            for(auto c: a){
+                b += c;
+                if(f2[b]) answer = max(answer, int(b.size()));
+            }
+        }
+        for(auto &i : arr2){
+            string a = to_string(i), b = "";
+            for(auto c: a){
+                b += c;
+                if(f1[b]) answer = max(answer, int(b.size()));
+            }
+        }
+        return answer;
+    }
+};
