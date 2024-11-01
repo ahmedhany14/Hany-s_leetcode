4fcbe81a0355738365fde4f7a62084d0a246e07b
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-11-02 01:29:01 +0200

    Time: 31 ms (45.26%), Space: 42.8 MB (63.82%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -1,7 +1,16 @@
-class Solution(object):
-    def makeFancyString(self, s):
-        result = ''
-        for char in s:
-            if not result[-2:] == char * 2:
-                result += char
-        return result
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string answer;;
+        answer.push_back(s[0]);
+        int seg = 0;
+        for(int i =  1; i < s.size(); ++i){
+            if(answer.back() == s[i]){
+                seg++;
+                if(seg < 2)  answer.push_back(s[i]);           
+            }
+            else answer.push_back(s[i]), seg = 0;
+        }
+        return answer;
+    }
+};
