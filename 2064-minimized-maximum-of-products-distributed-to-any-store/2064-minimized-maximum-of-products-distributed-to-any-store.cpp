20b597b4bbc1efe4cc7e2d1274e1f93e884e16eb
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-11-14 02:43:16 +0200

    Time: 131 ms (21.35%), Space: 87.4 MB (34.13%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+    
+    bool pile_spliting(vector<int>& quantities,int min_max, int temp_n){
+        for(auto i : quantities){
+            temp_n -= ceil(i * 1.0 / min_max);
+        }
+        return temp_n >= 0;
+    }
     int minimizedMaximum(int n, vector<int>& quantities) {
-        
-        int l = 1, r = *max_element(quantities.begin(), quantities.end()), best = 0;
+        int l = 1, r = 1e9, best = 0;
         while(l <= r){
-            int mid = (l + r) / 2, temp_n = n;
-            for(auto i : quantities) temp_n -= ceil(i * 1.0 / mid);
-            temp_n >= 0 ?  r = mid - 1, best = mid : l = mid + 1;
+            int mid = (l + r) / 2;
+            pile_spliting(quantities, mid, n) ?  r = mid - 1, best = mid : l = mid + 1;
         }
         return best;
     }
