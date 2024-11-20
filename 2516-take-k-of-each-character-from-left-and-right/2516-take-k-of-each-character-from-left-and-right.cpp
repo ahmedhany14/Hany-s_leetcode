058f576e7752df987119db495ff81126c2495521
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-11-20 03:11:02 +0200

    Time: 173 ms (12.68%), Space: 12.3 MB (40.06%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     
+    bool have_k(vector<int>& f, int& k){
+        if(f[0] >= k and f[1] >= k and f[2] >= k) return 1;
+        return 0;
+    }
     bool can(int& mid, string& s, int& k){
         int skip = s.size() - mid;
         vector<int> f(3, 0);
         for(int i = skip; i < s.size(); ++i) f[s[i] - 'a']++;
-        if(f[0] >= k and f[1] >= k and f[2] >= k) return 1;
+        if(have_k(f, k)) return 1;
         int l = 0, r = skip;
         while(r < s.size()){
             f[s[l++] - 'a']++;
             f[s[r++] - 'a']--;
-            if(f[0] >= k and f[1] >= k and f[2] >= k) return 1;
+            if(have_k(f, k)) return 1;
         }
         return 0;
     }
