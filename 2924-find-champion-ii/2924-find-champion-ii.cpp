de4d2633a6e3cecfc3596a9a3bc99a263c37b828
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-11-26 03:40:55 +0200

    Time: 45 ms (36.92%), Space: 104.1 MB (38.58%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     
-    int dfs(int node, vector<bool>& mark, vector<vector<int>>& adj){
-        int me = 0;
-        mark[node] = 1;
-        for(auto child: adj[node]) {
-            if(!mark[child])
-                me += dfs(child, mark, adj) + 1;
-        }
-        return me;
-    }
     
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n + 1);
-        for(auto i : edges) adj[i[0]].emplace_back(i[1]);
-    
+        
+        vector<int> deg(n + 1);
+        for(auto i : edges) deg[i[1]]++;
+        int winner = -1, cnt = 0;        
         for(int i = 0; i < n; ++i) {
-            vector<bool> mark(n + 1, false);
-            if(dfs(i, mark, adj) + 1 == n) return i;
+            if(deg[i] == 0){
+                cnt ++;
+                winner = i;
+            }
         }
-        return -1;
+        return cnt == 1 ? winner : -1;
     }
 };
 
