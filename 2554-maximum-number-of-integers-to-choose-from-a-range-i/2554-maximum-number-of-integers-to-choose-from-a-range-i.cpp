36059f4bdabc3530bafa63412911c85a0ebdf5ef
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-12-06 13:20:48 +0200

    Time: 123 ms (77.10%), Space: 145.8 MB (69.94%) - LeetHub

---
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        set<int> st;
+        for(auto i : banned) if(i <= n) st.insert(i);
+        int answer = 0;
+        for(int i = 1; i <= n and maxSum >= i; ++i){
+            if(st.contains(i) == 0 and maxSum >= i){
+                maxSum -= i;
+                answer++;
+            }
+        }
+        return answer;
+    }
+};
