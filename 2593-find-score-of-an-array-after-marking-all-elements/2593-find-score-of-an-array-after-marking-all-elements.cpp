88ba98dd1846e4c4c9034df6db2e1fde322680f6
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-12-13 16:48:52 +0200

    Time: 72 ms (80.14%), Space: 108.6 MB (38.24%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -5,12 +5,14 @@ public:
         for(int i = 0; i < nums.size(); ++i) p.emplace_back(nums[i], i);
         sort(p.begin(), p.end());
         long long score = 0;
-        map<int, int> mark;
 
+        vector<int> mark(nums.size() + 1);
         for(auto[val, index] : p){
             if(!mark[index]) {
                 score += val;
-                mark[index - 1] = mark[index + 1] = 1;
+                if(index - 1 >= 0)
+                    mark[index - 1] = 1;
+                mark[index + 1] = 1;
             }
         }        
         
