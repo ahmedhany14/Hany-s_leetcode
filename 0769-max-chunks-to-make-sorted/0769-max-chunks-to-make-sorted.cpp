4f20ce29a61e1ef0afde84d076173f8ad7eb7fe8
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-12-19 13:03:07 +0200

    Time: 0 ms (100.00%), Space: 9.3 MB (20.72%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int prefix = 0, accurat_sum = 0, segments = 0;
+        for(int i = 0; i < arr.size(); ++i){
+            prefix += arr[i];
+            accurat_sum += i;
+            segments += prefix == accurat_sum;
+        }
+        return segments;
+    }
+};
