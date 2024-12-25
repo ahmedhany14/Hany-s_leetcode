b0fb4a41ccb049b353e64eb86dd9cfbbaf641084
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-12-25 08:58:18 +0200

    Time: 0 ms (100.00%), Space: 23 MB (6.85%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -11,6 +11,15 @@
  */
 class Solution {
 public:
+    
+    int max_dep(TreeNode* root){
+        if(root == NULL) return 0;
+        int me = 0;
+        me = max(me, 1 + max_dep(root->right));
+        me = max(me, 1 + max_dep(root->left));
+        return me;
+    }
+    
     void dfs(TreeNode* root, int dep, vector<vector<int>>&depth){
         if(root == NULL) return;
         depth[dep].emplace_back(root->val);        
@@ -18,7 +27,7 @@ public:
         dfs(root->left, dep + 1, depth);
     }
     vector<int> largestValues(TreeNode* root) {
-        vector<vector<int>> depth (10004);
+        vector<vector<int>> depth (max_dep(root) + 2);
         dfs(root, 1, depth);
         vector<int> answer;        
         for(int i = 1; i < depth.size(); ++i){
